870ef9d57435e7990e7e1419d5051adb0250b1f4
Stephen Zhang <x.stephen.zhang@gmail.com>
2025-02-25 17:19:01 -0500

    Update multifisher.m

--- multifisher.m
+++ multifisher.m
@@ -1,9 +1,16 @@
-function pvalues = multifisher(data, comps)
+function [pvalues, ncomps] = multifisher(data, comps)
 % Fisher's exact test corrected for multiple comparisons
-% data - n x 2 matrix where the rows are independent variables, each row is
-%        entered as [hits, totals]
-% comps - 0: all rows compared to all other rows
-%         1-x: compare all rows to row 1-x
+% [pvalues, ncomps] = multifisher(data, comps)
+%
+% data -  n x 2 matrix where the rows are conditions, each row is entered 
+%         as [hits, totals]
+% comps - 0     (1D): all rows compared to all other rows
+%         1-x   (1D): compare all rows to row 1-x
+%         2D:   each row specify which two conditions are compared: 
+%               e.g., [Condtion1, Condition2; Condition1, Condition 3]
+% pvalues m x 3 matrix, where each row specify a fisher's exact test
+%         e.g., [Condition1, Condition2, p-value (inflated with Bonferroni]
+% ncomps  Number of comparisons
 
 if nargin < 2
     comps = 0;
@@ -16,21 +23,28 @@ datatable = array2table(data);
 nvar = size(datatable,1);
 
 % Comparason table
-switch comps
-    case 0
-        pvalues = zeros(nchoosek(nvar,2), 3);
-        ind = 0;
-        for i = 1 : nvar
-            for j = i + 1 : nvar
-                ind = ind + 1;
-                pvalues(ind,1) = i;
-                pvalues(ind,2) = j;
+if size(comps,2) == 2
+    % 2D
+    pvalues = comps;
+    pvalues(:,3) = 0;
+elseif size(comps,2) == 1
+    % 1D
+    switch comps
+        case 0
+            pvalues = zeros(nchoosek(nvar,2), 3);
+            ind = 0;
+            for i = 1 : nvar
+                for j = i + 1 : nvar
+                    ind = ind + 1;
+                    pvalues(ind,1) = i;
+                    pvalues(ind,2) = j;
+                end
             end
-        end
-    otherwise
-        pvalues = (1 : nvar)';
-        pvalues(pvalues == comps) = [];
-        pvalues = cat(2, ones(nvar-1,1) * comps, pvalues, zeros(nvar-1,1));
+        otherwise
+            pvalues = (1 : nvar)';
+            pvalues(pvalues == comps) = [];
+            pvalues = cat(2, ones(nvar-1,1) * comps, pvalues, zeros(nvar-1,1));
+    end
 end
 ncomps = size(pvalues, 1);
 
